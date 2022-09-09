aec11a152766339b9c6524bf35923637bf6c5d3d
VanessaMaia26 <vanessamaia2607@gmail.com>
2022-09-09 14:54:00 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = ()  => {
+  return (
+    <div>
+      <button style={{backgroundColor: '#FF0000', color: '#FFF', border: '0'}}>Clique aqui</button>
+    </div>
+  )
+}
+
+export default App;
